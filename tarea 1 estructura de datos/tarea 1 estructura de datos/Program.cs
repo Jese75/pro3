ca0e56056dcc05439e66c150cacbd6cab965430a
Jese75 <20221091@itla.edu.do>
2023-09-13 15:30:01 -0700

    prueba 1

---
+++ tarea 1 estructura de datos/tarea 1 estructura de datos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tarea_1_estructura_de_datos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
